0476ecfe95d92adedcf9a94ccef66953c70b7e38
Harlin Seritt <harlin.seritt@alfresco.com>
2023-02-05 07:54:10 -0700

    DEVL-34 - Fix bug: showing closed sprints in open sprints page

--- apps/ui/templatetags/ui_tags.py
+++ apps/ui/templatetags/ui_tags.py
@@ -16,5 +16,10 @@ def task_finished_status(task_qs):
     return task_qs.filter(CLOSED_QUERY).order_by(*SPRINT_TASK_ORDER)
 
 
+def sprint_open_status(sprint_qs):
+    return sprint_qs.filter(status="Open").order_by("-started")
+
+
 register.filter("task_open_status", task_open_status)
 register.filter("task_finished_status", task_finished_status)
+register.filter("sprint_open_status", sprint_open_status)
