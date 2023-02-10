182c08fdb19aa986bf41146f5213a497a31f70c1
Harlin Seritt <harlin.seritt@gmail.com>
2023-02-10 04:32:10 -0700

    DEVL-39 - Added an enhancement field for tasks

--- test-up.sh
+++ test-up.sh
@@ -2,5 +2,5 @@
 
 pipenv requirements > docker/requirements.txt
 cp -rf apps config manage.py staticfiles docker/.
-docker-compose -f docker/docker-compose.test.yml up --build -d
+docker-compose -f docker/docker-compose.test.yml up --build
 
